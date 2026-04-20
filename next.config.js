c3831958f7915fe3ebd545286b68117447317ad9
HawkEyeTheRapper <hawkeyetherapper@gmail.com>
2026-04-20 07:38:47 -0500

    [codex] Update store asset hosts to assets.omniversal.vip

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['hawkeyetherapper.shop', 'shop.omniversal.vip', 'assets.omniversal.vip'],
+    domains: ['hawkeyetherapper.shop', 'assets.omniversal.vip'],
   },
 };
 
